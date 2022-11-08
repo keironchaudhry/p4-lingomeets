02cc9e1b9fc42f2735bb10c02773bad9339bd8fd
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-11-08 11:45:10 +0000

    Modify views to render past event meetup onto template

--- event/views.py
+++ event/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import Event
 
 # Create your views here.
 
 
-class PastMeetups(generic.TemplateView):
+class PastMeetups(generic.ListView):
     """
     View for past meetups in website
     """
+    model = Event
+    queryset = Event.objects.order_by('-created_on')
     template_name = 'past_meetups.html'
+    paginate_by = 3
