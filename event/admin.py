b66917bd1e0198c7f5570a07b44145ceb5737a36
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-11-08 09:45:31 +0000

    Create eventadmin class for admin control over event feature

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    """
+    Admin control for event model
+    design feature for Lingomeets website
+    """
+    list_display = [
+        'title',
+        'slug',
+        'created_on'
+    ]
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = [
+        'created_on'
+    ]
