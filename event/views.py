26af43dd55d78be46e79ee21b0bba67772f6c5ba
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-11-08 16:53:50 +0000

    Create view to render meetup detail template

--- event/views.py
+++ event/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Event
 
 # Create your views here.
@@ -13,3 +13,21 @@ class PastMeetups(generic.ListView):
     queryset = Event.objects.order_by('-created_on')
     template_name = 'past_meetups.html'
     paginate_by = 3
+
+
+class MeetupView(View):
+    """
+    View for rendering meet up 
+    event detail page in website
+    """
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Event.objects
+        event = get_object_or_404(queryset, slug=slug)
+
+        return render(
+            request,
+            'meetup_detail.html',
+            {
+                'event': event,
+            }
+        )
