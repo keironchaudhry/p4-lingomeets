faad00098e5634ed07e66c4b9597efd5a8ac315c
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-11-08 09:30:47 +0000

    Create event model

--- event/models.py
+++ event/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Event(models.Model):
+    """
+    Main model for meet-up events
+    featured in Lingomeets website
+    """
+    title = models.CharField(
+        'Event Title',
+        max_length=200,
+        unique=True
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True
+    )
+    date = models.DateField(
+        'Event Date',
+        auto_now=False,
+        blank=False
+    )
+    destination = models.CharField(
+        'Event Destination',
+        max_length=200
+    )
+    image = CloudinaryField(
+        'Event Image',
+        default='placeholder'
+    )
+    description = models.TextField(
+        blank=True
+    )
+    price = models.IntegerField(
+        'Event Price',
+        default=0
+    )
+    attendees = models.ManyToManyField(
+        User,
+        related_name='attendee(s)',
+        blank=True
+    )
+    created_on = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    class Meta:
+        ordering = ['-created_on']
