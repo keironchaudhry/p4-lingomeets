844cd51d139e377f81993a6038b0467e013c86a8
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-11-08 16:59:57 +0000

    Create url for meetup detail in event app

--- event/urls.py
+++ event/urls.py
@@ -7,4 +7,5 @@ from . import views
 
 urlpatterns = [
     path('past_meetups/', views.PastMeetups.as_view(), name='past_meetups'),
+    path('<slug:slug>/', views.MeetupView.as_view(), name='meetup_detail'),
 ]
