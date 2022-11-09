af0ba6f6711b94ca5488a4d65f82446507ecf983
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-11-09 12:10:49 +0000

    Add review approval to admin review class

--- review/admin.py
+++ review/admin.py
@@ -8,6 +8,26 @@ class ReviewAdmin(admin.ModelAdmin):
     Admin control for review model
     featured in Lingomeets website
     """
-    list_display = ('event', 'user', 'rating', 'created_at')
-    list_filter = ('event', 'user', 'rating', 'created_at')
-    search_fields = ('user', 'event', 'rating')
+    list_display = (
+        'event',
+        'user',
+        'rating',
+        'created_at'
+    )
+    list_filter = (
+        'event',
+        'user',
+        'rating',
+        'created_at'
+    )
+    search_fields = (
+        'user',
+        'event',
+        'rating'
+    )
+    actions = [
+        'approve_comments'
+    ]
+
+    def approve_comments(self, request, queryset):
+        queryset.update(is_admin_approved=True)
