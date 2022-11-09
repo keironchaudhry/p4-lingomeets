368481a67dec16cb789474a18c859c7cd54f4216
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-11-09 18:11:04 +0000

    Render review form in views for meetup detail template

--- event/views.py
+++ event/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from review.models import Review
@@ -18,16 +19,19 @@ class PastMeetups(generic.ListView):
 
 class MeetupView(View):
     """
-    View for rendering meet up 
+    View for rendering meet up
     event detail page in website
     """
     def get(self, request, slug, *args, **kwargs):
+        """
+        GET method to get meetup post and reviews
+        """
         queryset = Event.objects
         event = get_object_or_404(queryset, slug=slug)
 
         review = Review.objects
         reviews = review.filter(is_admin_approved=True)
-        reviewed = True
+        reviewed = False
         review_form = ReviewForm()
 
         return render(
@@ -40,3 +44,40 @@ class MeetupView(View):
                 'review_form': review_form
             }
         )
+ 
+    def post(self, request, slug, *args, **kwargs):
+        """
+        POST method to post reviews
+        """
+        queryset = Event.objects
+        event = get_object_or_404(queryset, slug=slug)
+
+        review = Review.objects
+        reviews = review.filter(is_admin_approved=True)
+        reviewed = False
+        review_form = ReviewForm()
+
+        if (not reviewed):
+            if review_form.is_valid():
+                review_form.instance.email = request.user.email
+                review_form.instance.name = request.user.username
+                review = review_form.save(commit=False)
+                review.event = event
+                review.user = request.user
+                review.save()
+                return HttpResponseRedirect(
+                    reverse(
+                        'meetup_detail.html',
+                        args=[slug])
+                )
+            else:
+                return render(
+                    request,
+                    'meetup_detail.html',
+                    {
+                        'event': event,
+                        'reviews': reviews,
+                        'reviewed': reviewed,
+                        'review_form': review_form
+                    }
+                )
