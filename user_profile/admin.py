da2ef018eae1565084e7a0e6c3f7da0f270fdb2a
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-11-11 15:29:11 +0000

    Create profile admin class for admin control over user profiles

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'user',
+        'first_name',
+        'last_name',
+        'birthday',
+        'native_language',
+        'other_language',
+        'bio',
+        'created_at',
+        'updated_at'
+    )
+    list_filter = (
+        'user',
+        'birthday',
+        'native_language',
+        'other_language',
+        'created_at',
+        'updated_at'
+    )
+    search_fields = (
+        'user',
+        'native_language',
+        'other_language'
+    )
