3b9e2216089f77687ec3217814505db4d756b6bf
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-11-11 15:37:59 +0000

    Create user_profile template and userprofile class in views

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class UserProfile(generic.ListView):
+    """
+    View for displaying user profile
+    """
+    model = Profile
+    queryset = Profile.objects.all()
+    template_name = 'user_profile.html'
