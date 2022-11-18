0fa85c21aaddd06aaa324c3d8832f14dc113484b
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-11-18 09:48:34 +0000

    Further organise code in settings.py

--- lingomeets/settings.py
+++ lingomeets/settings.py
@@ -20,7 +20,9 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATES_DIR = os.path.join(
+    BASE_DIR, 'templates'
+)
 
 
 # Quick-start development settings - unsuitable for production
@@ -164,8 +166,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(
+    BASE_DIR, 'static'
+    )
+]
+STATIC_ROOT = os.path.join(
+    BASE_DIR, 'staticfiles'
+)
 
 
 MEDIA_URL = '/media/'
